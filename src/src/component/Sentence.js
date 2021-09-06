14f2bb595dbbf6855194673b782a7943f0e8927a
阳光 <qingxianyang@outlook,com>
2021-09-06 22:23:47 +0800

    独立化sentence模块

--- src/src/component/Sentence.js
+++ src/src/component/Sentence.js
@@ -22,10 +22,6 @@ class Sentence extends React.Component {
         this.handleValueChange = this.handleValueChange.bind(this);
         this.updateMarkdownHtml = this.updateMarkdownHtml.bind(this);
         this.saveSentence = this.saveSentence.bind(this);
-        this.addWords=this.addWords.bind(this);
-        this.deleteWord=this.deleteWord.bind(this);
-        this.updateWord=this.updateWord.bind(this);
-        this.updateWordDetail=this.updateWordDetail.bind(this);
     }
 
     getSentenceDetail(id) {
@@ -95,49 +91,6 @@ class Sentence extends React.Component {
         })
     }
 
-    addWords(){
-        let words=this.state.wordsList;
-        words[this.state.wordsCount]={
-            id:0,
-            word:''
-        };
-        this.setState({
-            wordsList:words,
-            wordsCount:this.state.wordsCount+1
-        });
-    }
-    deleteWord(index){
-        let words=[];
-        let count=0;
-        for (let i=0;i<this.state.wordsCount;i++){
-            if(i!==index){
-                words[count]=this.state.wordsList[i];
-                count++;
-            }
-        }
-        this.setState({
-            wordsList:words,
-            wordsCount:count
-        });
-    }
-
-    updateWord(event,index){
-        let words=this.state.wordsList;
-        words[index].value=event.target.value;
-        this.setState({
-            wordsList:words
-        });
-    }
-
-    updateWordDetail(index){
-        this.setState({
-            updateWord:this.state.wordsList[index].value
-        });
-        this.setState({
-            visible:true
-        });
-    }
-
     render() {
         // note 部分管理
         var notePart = <div></div>;
@@ -154,130 +107,55 @@ class Sentence extends React.Component {
                 <div id={this.state.sentenceData.ID + "_sentence_note"}></div>
             </div>;
         }
-        // words部分管理
-        let words=this.state.wordsList.map((value,index)=>{
-            return <Row key={index} justify="center" alian={"center"} style={{paddingTop:"10px"}}>
-                        <Col span={19}>
-                            <Input
-                                value={this.state.wordsList[index].value}
-                                style={{height:"100%"}}
-                                onChange={(e)=>this.updateWord(e,index)}
-                            />
-                        </Col>
-                        <Col offset={1} span={4}>
-                            <Button
-                                size={"small"}
-                                type={"link"}
-                                shape={"round"}
-                                icon={<EditOutlined />}
-                                onClick={()=>this.updateWordDetail(index)}
-                            >
-                            </Button>
-                            <Button
-                                icon={<DeleteOutlined />}
-                                size={"small"}
-                                type={"link"}
-                                shape={"round"}
-                                onClick={()=>this.deleteWord(index)}
-                            >
-                            </Button>
-                        </Col>
-                    </Row>
-        })
         return (
             <div className="container">
-                <Row>
-                    <Col span={15}>
-                        <div className="row">
-                            <h3>ID:{this.state.sentenceData.ID}</h3>
-                            <h3>AddTime:{this.state.sentenceData.AddTime}</h3>
-                            <h3>LastUpdateTime:{this.state.sentenceData.LastUpdateTime}</h3>
-                        </div>
-                        <div className="row">
-                            <Form
-                                layout="vertical"
-                            >
-                                <Form.Item label="Sentence">
-                                    <Input
-                                        placeholder="Sentence"
-                                        value={this.state.sentenceData.sentence}
-                                        onChange={(e) => this.handleValueChange(e, 'sentence')}
-                                    />
-                                </Form.Item>
-                                <Form.Item label="Source">
-                                    <Input
-                                        placeholder="Source"
-                                        value={this.state.sentenceData.source}
-                                        onChange={(e) => this.handleValueChange(e, 'source')}
-                                    />
-                                </Form.Item>
-                                <Form.Item label="Note">
-                                    <Button
-                                        icon={<EditOutlined/>}
-                                        type="primary"
-                                        onClick={() => {
-                                            this.setState({
-                                                editNote: !this.state.editNote
-                                            })
-                                        }}
-                                    >
-                                        {this.state.editNote ? 'Finish' : 'Edit'}
-                                    </Button>
-                                    {notePart}
-                                </Form.Item>
-                                <Form.Item>
-                                    <Button
-                                        icon={<SaveOutlined/>}
-                                        type="primary"
-                                        onClick={() => this.saveSentence()}
-                                    >
-                                        Save
-                                    </Button>
-                                </Form.Item>
-                            </Form>
-                        </div>
-                    </Col>
-                    <Col offset={1} span={8}>
-                        <Row>
+                <div className="row">
+                    <h3>ID:{this.state.sentenceData.ID}</h3>
+                    <h3>AddTime:{this.state.sentenceData.AddTime}</h3>
+                    <h3>LastUpdateTime:{this.state.sentenceData.LastUpdateTime}</h3>
+                </div>
+                <div className="row">
+                    <Form
+                        layout="vertical"
+                    >
+                        <Form.Item label="Sentence">
+                            <Input
+                                placeholder="Sentence"
+                                value={this.state.sentenceData.sentence}
+                                onChange={(e) => this.handleValueChange(e, 'sentence')}
+                            />
+                        </Form.Item>
+                        <Form.Item label="Source">
+                            <Input
+                                placeholder="Source"
+                                value={this.state.sentenceData.source}
+                                onChange={(e) => this.handleValueChange(e, 'source')}
+                            />
+                        </Form.Item>
+                        <Form.Item label="Note">
                             <Button
-                                icon={<FormOutlined />}
+                                icon={<EditOutlined/>}
                                 type="primary"
-                                onClick={()=>this.addWords()}
+                                onClick={() => {
+                                    this.setState({
+                                        editNote: !this.state.editNote
+                                    })
+                                }}
                             >
-                                New Words
+                                {this.state.editNote ? 'Finish' : 'Edit'}
                             </Button>
-                        </Row>
-                        <Row>
-                            {words}
-                        </Row>
-                        <Row style={{marginTop:"10px"}}>
+                            {notePart}
+                        </Form.Item>
+                        <Form.Item>
                             <Button
                                 icon={<SaveOutlined/>}
                                 type="primary"
+                                onClick={() => this.saveSentence()}
                             >
-                                Save Words
+                                Save
                             </Button>
-                        </Row>
-                    </Col>
-                </Row>
-                <div>
-                    <Modal
-                        visible={this.state.visible}
-                        onCancel={()=>{
-                            this.setState({
-                                visible:false
-                            })
-                        }}
-                        onOk={()=>{
-                            this.setState({
-                                visible:false
-                            })
-                        }}
-                    >
-                        <WordModel
-                            word={this.state.updateWord}
-                        />
-                    </Modal>
+                        </Form.Item>
+                    </Form>
                 </div>
             </div>
         )
--- src/src/page/SentenceEdit.js
+++ src/src/page/SentenceEdit.js
@@ -3,14 +3,97 @@ import Header from "../component/Header";
 import Roads from "../component/Roads";
 import config from "../config/setting";
 import Sentence from "../component/Sentence";
+import {Button, Col, Input, Modal, Row} from "antd";
+import {DeleteOutlined, EditOutlined,FormOutlined,SaveOutlined} from "@ant-design/icons";
+import WordModel from "../component/WordModel";
+
 class SentenceEdit extends React.Component{
     constructor(props) {
         super(props);
         this.state={
-            id:props.match.params.id
+            id:props.match.params.id,
+            wordsList:[],
+            wordsCount:0,
+            visible:false,
+            updateWord:''
         }
+        this.addWords=this.addWords.bind(this);
+        this.deleteWord=this.deleteWord.bind(this);
+        this.updateWord=this.updateWord.bind(this);
+        this.updateWordDetail=this.updateWordDetail.bind(this);
+    }
+    addWords(){
+        let words=this.state.wordsList;
+        words[this.state.wordsCount]={
+            id:0,
+            word:''
+        };
+        this.setState({
+            wordsList:words,
+            wordsCount:this.state.wordsCount+1
+        });
+    }
+    deleteWord(index){
+        let words=[];
+        let count=0;
+        for (let i=0;i<this.state.wordsCount;i++){
+            if(i!==index){
+                words[count]=this.state.wordsList[i];
+                count++;
+            }
+        }
+        this.setState({
+            wordsList:words,
+            wordsCount:count
+        });
+    }
+
+    updateWord(event,index){
+        let words=this.state.wordsList;
+        words[index].value=event.target.value;
+        this.setState({
+            wordsList:words
+        });
+    }
+
+    updateWordDetail(index){
+        this.setState({
+            updateWord:this.state.wordsList[index].value
+        });
+        this.setState({
+            visible:true
+        });
     }
     render(){
+        let words=this.state.wordsList.map((value,index)=>{
+            return <Row key={index} justify="center" alian={"center"} style={{paddingTop:"10px"}}>
+                <Col span={19}>
+                    <Input
+                        value={this.state.wordsList[index].value}
+                        style={{height:"100%"}}
+                        onChange={(e)=>this.updateWord(e,index)}
+                    />
+                </Col>
+                <Col offset={1} span={4}>
+                    <Button
+                        size={"small"}
+                        type={"link"}
+                        shape={"round"}
+                        icon={<EditOutlined />}
+                        onClick={()=>this.updateWordDetail(index)}
+                    >
+                    </Button>
+                    <Button
+                        icon={<DeleteOutlined />}
+                        size={"small"}
+                        type={"link"}
+                        shape={"round"}
+                        onClick={()=>this.deleteWord(index)}
+                    >
+                    </Button>
+                </Col>
+            </Row>
+        })
         return(
             <div className="container">
                 <div className="row">
@@ -21,10 +104,53 @@ class SentenceEdit extends React.Component{
                         roads={config.common_road}
                     />
                 </div>
-                <div className="row">
-                    <Sentence
-                        id={this.state.id}
-                    />
+                <Row>
+                    <Col span={15}>
+                        <Sentence
+                            id={this.state.id}
+                        />
+                    </Col>
+                    <Col offset={1} span={8}>
+                        <Row>
+                            <Button
+                                icon={<FormOutlined />}
+                                type="primary"
+                                onClick={()=>this.addWords()}
+                            >
+                                New Words
+                            </Button>
+                        </Row>
+                        <Row>
+                            {words}
+                        </Row>
+                        <Row style={{marginTop:"10px"}}>
+                            <Button
+                                icon={<SaveOutlined/>}
+                                type="primary"
+                            >
+                                Save Words
+                            </Button>
+                        </Row>
+                    </Col>
+                </Row>
+                <div>
+                    <Modal
+                        visible={this.state.visible}
+                        onCancel={()=>{
+                            this.setState({
+                                visible:false
+                            })
+                        }}
+                        onOk={()=>{
+                            this.setState({
+                                visible:false
+                            })
+                        }}
+                    >
+                        <WordModel
+                            word={this.state.updateWord}
+                        />
+                    </Modal>
                 </div>
             </div>
         );
