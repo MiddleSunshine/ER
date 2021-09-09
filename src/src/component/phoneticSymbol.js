c1755559d81dead20d9b08392a7b9d75884db353
阳光 <qingxianyang@outlook,com>
2021-09-09 22:17:46 +0800

    系统基础架构完成

--- src/src/component/phoneticSymbol.js
+++ src/src/component/phoneticSymbol.js
@@ -1,6 +1,8 @@
 import React from "react";
 import config from "../config/setting";
 import "../css/symbolKeybord.css"
+import {Button} from "antd";
+import {FormOutlined,AudioOutlined} from "@ant-design/icons";
 
 // https://www.hujiang.com/c/wx/p1288247/
 var symbolMap={
@@ -268,83 +270,125 @@ class PhoneticSymbol extends React.Component{
     constructor(props) {
         super(props);
         this.state={
-            media:''
+            media:'',
+            symbols:[]
         }
         this.playMp3=this.playMp3.bind(this);
+        this.updateWords=this.updateWords.bind(this);
+        this.saveWords=this.saveWords.bind(this);
     }
-    playMp3(source){
+    playMp3(source,img){
         console.log(source);
+        let symbol=img.slice(1,-1);
+        let symbols=this.state.symbols;
+        symbols.push(symbol);
         this.setState({
-            media:config.front_domain+"/media/"+source
+            media:config.front_domain+"/media/"+source,
+            symbols: symbols
         });
+    }
+    updateWords(index){
+        let newSymbols=this.state.symbols.filter((current,i)=>{
+            return i!==index;
+        });
+        this.setState({
+            symbols:newSymbols
+        })
+    }
+    saveWords(){
+        let symbols=this.state.symbols.join(",");
+        sessionStorage.setItem("symbols",symbols);
     }
     render() {
         return(
             <div className={"table-responsive"}>
-                <table className={"table table-hover table-condensed symbol-keybord"}>
-                    <tr>
-                        <td rowSpan={3}>元音</td>
-                        <td rowSpan={2}>单元音</td>
-                        <td>长元音</td>
-                        <td>
-                            {symbolMap.yuanyin.danyuanyin.changyuanyin.map((item)=>{
-                                return <span onClick={()=>this.playMp3(item.media)} key={item.id}>{item.img}</span>
-                            })}
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>短元英</td>
-                        <td>
-                            {
-                                symbolMap.yuanyin.danyuanyin.duanyuanyin.map((item)=>{
-                                    return <span onClick={()=>this.playMp3(item.media)} key={item.id}>{item.img}</span>
-                                })
-                            }
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>双元音</td>
-                        <td>
-                            {symbolMap.yuanyin.shuangyuanyin.map((item)=>{
-                                return <span onClick={()=>this.playMp3(item.media)} key={item.id}>{item.img}</span>
-                            })}
-                        </td>
-                    </tr>
-                    <tr>
-                        <td rowSpan={2} colSpan={2}>
-                            辅音
-                        </td>
-                        <td>
-                            清辅音
-                        </td>
-                        <td>
-                            {symbolMap.fuyin.qitafuyin.map((item)=>{
-                                return <span onClick={()=>this.playMp3(item.media)} key={item.id}>{item.img}</span>
-                            })}
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            浊辅音
-                        </td>
-                        <td>
-                            {symbolMap.fuyin.zhuofuyin.map((item)=>{
-                                return <span onClick={()=>this.playMp3(item.media)} key={item.id}>{item.img}</span>
-                            })}
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            其他辅音
-                        </td>
-                        <td>
-                            {symbolMap.fuyin.qitafuyin.map((item)=>{
-                                return <span onClick={()=>this.playMp3(item.media)} key={item.id}>{item.img}</span>
-                            })}
-                        </td>
-                    </tr>
-                </table>
-                <audio controls={"controls"} src={this.state.media} autoplay={"autoplay"}>        
+                <div>
+                    <Button
+                        style={{marginRight:"10px"}}
+                        type={"primary"}
+                        icon={<FormOutlined />}
+                        onClick={()=>this.saveWords()}
+                    >
+                        Save
+                    </Button>
+                    <Button
+                        icon={<AudioOutlined />}
+                        type={"primary"}
+                        style={{marginRight:"10px"}}
+                    >
+                    </Button>
+                    <span>/</span>
+                    {this.state.symbols.map((item,index)=>{
+                        return <span onClick={()=>this.updateWords(index)}>{item}</span>;
+                    })}
+                    <span>/</span>
+                </div>
+                <div>
+                    <table className={"table table-hover table-condensed symbol-keybord"}>
+                        <tr>
+                            <td className={"symbol"} rowSpan={3}>元音</td>
+                            <td className={"symbol"} rowSpan={2}>单元音</td>
+                            <td className={"symbol"}>长元音</td>
+                            <td>
+                                {symbolMap.yuanyin.danyuanyin.changyuanyin.map((item)=>{
+                                    return <span onClick={()=>this.playMp3(item.media,item.img)} key={item.id}>{item.img}</span>
+                                })}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className={"symbol"}>短元英</td>
+                            <td>
+                                {
+                                    symbolMap.yuanyin.danyuanyin.duanyuanyin.map((item)=>{
+                                        return <span onClick={()=>this.playMp3(item.media,item.img)} key={item.id}>{item.img}</span>
+                                    })
+                                }
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className={"symbol"}>双元音</td>
+                            <td colSpan={2}>
+                                {symbolMap.yuanyin.shuangyuanyin.map((item)=>{
+                                    return <span onClick={()=>this.playMp3(item.media,item.img)} key={item.id}>{item.img}</span>
+                                })}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className={"symbol"} rowSpan={2}>
+                                辅音
+                            </td>
+                            <td className={"symbol"}>
+                                清辅音
+                            </td>
+                            <td colSpan={2}>
+                                {symbolMap.fuyin.qitafuyin.map((item)=>{
+                                    return <span onClick={()=>this.playMp3(item.media,item.img)} key={item.id}>{item.img}</span>
+                                })}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className={"symbol"}>
+                                浊辅音
+                            </td>
+                            <td colSpan={2}>
+                                {symbolMap.fuyin.zhuofuyin.map((item)=>{
+                                    return <span onClick={()=>this.playMp3(item.media,item.img)} key={item.id}>{item.img}</span>
+                                })}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className={"symbol"}>
+                                其他辅音
+                            </td>
+                            <td colSpan={3}>
+                                {symbolMap.fuyin.qitafuyin.map((item)=>{
+                                    return <span onClick={()=>this.playMp3(item.media,item.img)} key={item.id}>{item.img}</span>
+                                })}
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <audio style={{display:"none"}} controls={"controls"} src={this.state.media} autoplay={"autoplay"}>
                 </audio>
             </div>
         )
